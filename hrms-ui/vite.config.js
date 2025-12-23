ca87c9ad58c27ee282fedbd1bb01cbce92f7e4b6
kudikalar <rameshkudikala72@gmail.com>
2025-12-23 21:04:17 +0530

    updated

--- hrms-ui/vite.config.js
+++ hrms-ui/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  // 🔴 REQUIRED FOR GITHUB PAGES
+  // ✅ MUST MATCH REPO NAME
   base: "/prakuratms/",
 
   build: {
