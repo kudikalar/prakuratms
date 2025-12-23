8bf5749c1df131dc19a46ae6d7567b5eb546d259
kudikalar <rameshkudikala72@gmail.com>
2025-12-23 20:40:16 +0530

    updated main

--- hrms-ui/src/main.jsx
+++ hrms-ui/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import "./index.css";
 const params = new URLSearchParams(window.location.search);
 const redirect = params.get("redirect");
 
--- hrms-ui/vite.config.js
+++ hrms-ui/vite.config.js
@@ -4,11 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  // 🔴 THIS IS REQUIRED FOR GITHUB PAGES
+  // 🔴 REQUIRED FOR GITHUB PAGES
   base: "/prakuratms/",
 
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    cssCodeSplit: true,
   },
 });
