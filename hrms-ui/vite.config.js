4c13382a7a93bde6ebef126e18f677dc01bea9c2
kudikalar <rameshkudikala72@gmail.com>
2025-12-26 16:04:33 +0530

    Update vite.config.js

--- hrms-ui/vite.config.js
+++ hrms-ui/vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/hrms-ui/",
+  base: "/prakuratms/", // ✅ MUST match repo name exactly
 });
-
